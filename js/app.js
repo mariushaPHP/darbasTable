5e42162398efd7995f3ecee4e5e5262ee31f59a5
mariushaPHP <90642649+mariushaPHP@users.noreply.github.com>
2021-11-17 18:49:13 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+'use strict'
+
+let text = "Man labai patinka JavaScript 😃" ;
+
+document.querySelector('.button').addEventListener('click', ()=>{
+
+    let rowNr = document.querySelector('.row').value;
+    let columnNr = document.querySelector('.column').value;
+    const table = document.createElement('table');
+    document.querySelector('body').appendChild(table);
+
+    for(let i=0; i<rowNr; i++){
+        const row = document.createElement('tr')
+        for(let i=0; i<columnNr; i++){
+            const cell = document.createElement('td');
+            cell.textContent = text;
+            row.appendChild(cell)
+        }
+        table.appendChild(row);
+    }
+});
